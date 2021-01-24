ac37bda41fb9138cc4ef0b0feede1698972a94ce
springdsc <spring_cainiao@163.com>
2021-01-24 21:48:36 +0800

    21:48

--- src/main/java/com/dsc/service/HelloWorldService.java
+++ src/main/java/com/dsc/service/HelloWorldService.java
@@ -1,12 +1,28 @@
 package com.dsc.service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @Classname HelloWorldService
  * @Description TODO
  * @Date 2021/1/24 20:30
  * @Created by 撒破云
  */
+@RestController
+@EnableAutoConfiguration
 public class HelloWorldService {
 
+    @RequestMapping("/getUser")
+    public String getUser(){
+        return "dsc";
+    }
 
+    public static void main(String[] args) {
+        String aa = "";
+        String bb = "master";
+        SpringApplication.run(HelloWorldService.class,args);
+    }
 }
